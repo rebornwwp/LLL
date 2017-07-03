13a6984f828fd41d6565813549beb41c61ca4bdb
Allen <rebornwwp@gmail.com>
2017-07-03 15:15:16 +0800

    修改代码到基于class view，无Mixins

--- rest/rest_learn/snippets/urls.py
+++ rest/rest_learn/snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
