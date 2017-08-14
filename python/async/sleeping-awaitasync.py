11abe1d7c748b02503e3ee285a3584bbadf01163
Allen <rebornwwp@gmail.com>
2017-08-14 16:01:53 +0800

    一些异步代码

---
+++ python/async/sleeping-awaitasync.py
@@ -0,0 +1,17 @@
+import asyncio
+
+async def slow_operation(n):
+    await asyncio.sleep(1)
+    print("Slow operation {} complete".format(n))
+
+
+async def main():
+    await asyncio.wait([
+        slow_operation(1),
+        slow_operation(2),
+        slow_operation(3),
+    ])
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
