d3d230cabf0d528a183767830fe4629177f6cb01
wang <rebornwwp@gmail.com>
2017-08-22 01:15:13 +0800

    python note

---
+++ python/modules.py
@@ -0,0 +1,9 @@
+"""
+这是一个modules的__doc__, __name__, __file__的实验.
+"""
+if __name__ == '__main__':
+    print(__doc__)
+    print(__name__)
+    print(__file__)
+    print(__import__)
+    print(__debug__)
