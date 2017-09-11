fcce9bfad069f9a2a2abe866a3ea00a56b2d9e2d
wang <rebornwwp@gmail.com>
2017-09-11 18:06:22 +0800

    leetcode

--- algorithm/Linked-List-Cycle.py
+++ algorithm/Linked-List-Cycle.py
@@ -4,9 +4,19 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution(object):
     def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
+        if head is None or head.next is None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
