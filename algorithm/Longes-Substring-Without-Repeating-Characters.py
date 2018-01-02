bf0bd05146fe5d82d2aa22462d35272bf8376b41
wanpeng <wanpeng@guanghe.tv>
2018-01-02 11:11:04 +0800

    leetcode

---
+++ algorithm/Longes-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = maxLength = 0
+        usedChar = {}
+
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+            usedChar[s[i]] = i
+        return maxLength
+
+
+def main():
+    solution = Solution()
+    s = "abcabcbb"
+    print(solution.lengthOfLongestSubstring(s))
+
+
+if __name__ == '__main__':
+    main()
