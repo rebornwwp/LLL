dbd8e8d65a9f4a5c2c1e13c424c7734e27788bbd
wanpeng <rebornwwp@gmail.com>
2018-08-09 18:10:05 +0800

    hackerrank

---
+++ haskell/hackerRank/Sum-of-Odd-Elements.hs
@@ -0,0 +1,8 @@
+f arr = sum [x | x <- arr, odd x]
+
+f' = sum . filter odd
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+  inputdata <- getContents
+  putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
