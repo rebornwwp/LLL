d5b37f31ae87dd05c9d52844ebd4808341b9cfe1
wanpeng <rebornwwp@gmail.com>
2018-09-22 00:15:52 +0800

    python学习

---
+++ python/http/httpServer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+import time
+
+
+class HttpServer(object):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
