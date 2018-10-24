89a897c8580518928722875153dd4e023ef4124a
wanpeng <rebornwwp@gmail.com>
2018-10-24 11:49:58 +0800

    learn typescript

---
+++ typescript/tricks/javascript-namespaces.ts
@@ -0,0 +1,12 @@
+namespace Utility {
+  export function log(msg) {
+    console.log(msg)
+  }
+
+  export function error(msg) {
+    console.error(msg)
+  }
+}
+
+Utility.log('Call me')
+Utility.error('maybe!')
