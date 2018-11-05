95001d9a5868bd9591f3f082fbafc9a4676c8268
wanpeng <rebornwwp@gmail.com>
2018-11-05 23:29:56 +0800

    再次刷题

--- algorithm/Same-Tree.py
+++ algorithm/Same-Tree.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -17,6 +18,31 @@ class Solution(object):
         elif q is None:
             return False
         if q.val == p.val:
-            return self.isSameTree(p.left, q.left) & self.isSameTree(p.right, q.right)
+            return self.isSameTree(p.left, q.left) & self.isSameTree(
+                p.right, q.right)
         else:
             return False
+
+    def isSameTree1(self, p, q):
+        if p is None and q is not None:
+            return False
+        if p is not None and q is None:
+            return False
+        if p is None and q is None:
+            return True
+        else:
+            return p.val == q.val and self.isSameTree1(
+                p.left, q.left) and self.isSameTree1(p.right, q.right)
+
+    def isSameTree2(self, a, b):
+        stack = [(a, b)]
+        while stack:
+            (p, q) = stack.pop()
+            if p and q and p.val == q.val:
+                stack.append((p.left, q.left))
+                stack.append((p.right, q.right))
+            elif not p and not q:
+                continue
+            else:
+                return False
+        return True
--- algorithm/Search-Insert-Position.py
+++ algorithm/Search-Insert-Position.py
@@ -11,7 +11,26 @@ class Solution(object):
             if nums[i] >= target:
                 return i
 
+    def searchInsert1(self, nums, target):
+        if nums[-1] < target:
+            return len(nums)
+        if nums[0] >= target:
+            return 0
+        return self.binary_find(nums, 0, len(nums) - 1, target)
+
+    def binary_find(self, nums, i, j, target):
+        mid = (i + j) // 2
+        if nums[mid + 1] >= target and nums[mid] < target:
+            return mid + 1
+        elif nums[mid] < target:
+            return self.binary_find(nums, mid + 1, j, target)
+        elif nums[mid] >= target:
+            return self.binary_find(nums, i, mid, target)
+
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.searchInsert([1,2,4], 3))
+    print(solution.searchInsert([1, 3, 5, 6], 2))
+    print(solution.searchInsert1([1, 3, 5, 6], 2))
+    print(solution.searchInsert1([1, 3], 2))
+    print(solution.searchInsert1([1, 3, 5], 3))
