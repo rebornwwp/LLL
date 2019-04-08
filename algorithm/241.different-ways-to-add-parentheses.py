a93cca0b0de19d21b3041207ef422e593dca66ce
wanpeng <rebornwwp@gmail.com>
2019-04-08 22:28:19 +0800

    add

---
+++ algorithm/241.different-ways-to-add-parentheses.py
@@ -0,0 +1,64 @@
+# encoding=utf8
+#
+# @lc app=leetcode id=241 lang=golang
+#
+# [241] Different Ways to Add Parentheses
+#
+# https://leetcode.com/problems/different-ways-to-add-parentheses/description/
+#
+# algorithms
+# Medium (49.22%)
+# Total Accepted:    70.4K
+# Total Submissions: 142.9K
+# Testcase Example:  '"2-1-1"'
+#
+# Given a string of numbers and operators, return all possible results from
+# computing all the different possible ways to group numbers and operators.
+# The valid operators are +, - and *.
+#
+# Example 1:
+#
+#
+# Input: "2-1-1"
+# Output: [0, 2]
+# Explanation:
+# ((2-1)-1) = 0
+# (2-(1-1)) = 2
+#
+# Example 2:
+#
+#
+# Input: "2*3-4*5"
+# Output: [-34, -14, -10, -10, 10]
+# Explanation:
+# (2*(3-(4*5))) = -34
+# ((2*3)-(4*5)) = -14
+# ((2*(3-4))*5) = -10
+# (2*((3-4)*5)) = -10
+# (((2*3)-4)*5) = 10
+#
+#
+
+
+class Solution:
+    def diffWaysToCompute(self, input):
+        def dfs(input, ans):
+            if '-' not in input and '+' not in input and '*' not in input:
+                return int(input)
+            ans = []
+            for i, val in enumerate(input):
+                if val == '-':
+                    ans.append(dfs(
+                        input[0:i]) - self.diffWaysToCompute(input[i+1:])))
+                if val == '+':
+                    ans.append(self.diffWaysToCompute(
+                        input[0:i]) + self.diffWaysToCompute(input[i+1:]))
+                if val == '*':
+                    ans.append(self.diffWaysToCompute(
+                        input[0:i]) * self.diffWaysToCompute(input[i+1:]))
+        ans = []
+        dfs(input, ans)
+        return ans
+
+s = Solution()
+print(s.diffWaysToCompute("2-1-1"))
