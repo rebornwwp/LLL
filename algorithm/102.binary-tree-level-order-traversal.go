f01f0083e69c9fc82bb56475e1735dfb2eda2e33
wanpeng <rebornwwp@gmail.com>
2019-04-15 22:18:44 +0800

    leetcode

---
+++ algorithm/102.binary-tree-level-order-traversal.go
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=102 lang=golang
+ *
+ * [102] Binary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (47.82%)
+ * Total Accepted:    358.5K
+ * Total Submissions: 749.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ *
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ *
+ *
+ * return its level order traversal as:
+ *
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("ehl")
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	var dfs func(*TreeNode, int)
+
+	dfs = func(root *TreeNode, level int) {
+		if root == nil {
+			return
+		}
+
+		if level >= len(res) {
+			res = append(res, []int{})
+		}
+		res[level] = append(res[level], root.Val)
+		dfs(root.Left, level+1)
+		dfs(root.Right, level+1)
+	}
+	dfs(root, 0)
+	return res
+}
