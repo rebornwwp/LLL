a6d5574e05347345b9f6c740a5215fce1396d361
wanpeng <rebornwwp@gmail.com>
2019-04-26 01:08:06 +0800

    leetcode

---
+++ algorithm/Reverse-Linked-List.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//  Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println(nil)
+}
+
+// func reverseList(head *ListNode) *ListNode {
+// 	var helper func(*ListNode, *ListNode)
+// 	helper = func(rList *ListNode, list *ListNode) {
+
+// 	}
+// }
