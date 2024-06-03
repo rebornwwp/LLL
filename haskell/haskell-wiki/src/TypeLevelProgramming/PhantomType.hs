97fd9571431aa8491ac50725c736f51e06c8375e
wanpeng.wang <rebornwwp@gmail.com>
2024-06-04 00:10:18 +0800

    fix: add

--- haskell/haskell-wiki/src/TypeLevelProgramming/PhantomType.hs
+++ haskell/haskell-wiki/src/TypeLevelProgramming/PhantomType.hs
@@ -8,25 +8,21 @@
 
 module TypeLevelProgramming.PhantomType where
 
-import           Data.Kind  (Type)
 import           Data.Proxy (Proxy (..))
 
-{- | https://www.stevenleiva.com/posts/phantom_types
--}
--- Mile and Kilometer 没有data constructor，这样这两个参数在value的level层面是phantom的存在
+{- | https://www.stevenleiva.com/posts/phantom_types -}
+-- Mile and Kilometer 没有data constructor，这样这两个参数在value level层面是phantom的存在
 -- 但是在type-level这两个参数是明显的存在的
 data Mile
 
 data Kilometer
-
 
 -- a is phantom parameters，通过types传入额外的信息
 newtype Distance a =
   Distance
     { unDistance :: Double
     }
-  deriving (Num)
-
+  deriving (Eq, Num)
 
 -- UnitName typeclass 通过 Proxy 感知到level level的PhantomType
 -- 输入一个type-level的值，输出term-level的值
@@ -34,7 +30,6 @@ newtype Distance a =
 -- Kind variable k means any kind. k can be a Type, Type -> Type or others
 class UnitName (u :: k) where
   unitName :: Proxy u -> String
-
 
 -- 下面两个可以让 PhantomType可以在unitName上执行
 instance UnitName Mile where
@@ -45,25 +40,20 @@ instance UnitName Kilometer where
   unitName :: Proxy Kilometer -> String
   unitName _ = "Km"
 
-
 -- 此时 unit type variable 可以在下面的函数体中使用
 -- 只是 在对 Distance做一层封装，获取到 Phantomtype
 -- 下面可以让unitName可以在 Distance unit执行
-instance UnitName unit => UnitName (Distance unit)
- where
+instance UnitName unit => UnitName (Distance unit) where
   unitName _ = unitName (Proxy :: Proxy unit)
-
 
 -- 与下面UnitName冲突所以注释掉
 -- instance UnitName unit => Show (Distance unit)
 --  where
 --   show (Distance u) = show u ++ "°" ++ unitName (Proxy :: Proxy unit)
-
 -- introduce a new unit, 这里的这种方式可以让代码易扩展
 data Danwei
 
-instance UnitName Danwei
- where
+instance UnitName Danwei where
   unitName _ = "Danwei"
 
 unit ::
@@ -75,7 +65,6 @@ unit _ = unitName (Proxy :: Proxy u)
 instance UnitName Distance where
   unitName :: Proxy Distance -> String
   unitName _ = "_unspecified unit_"
-
 
 -- visible type application
 -- read: forall {a}. Read a => String -> a
@@ -104,8 +93,7 @@ instance UnitNameAlt Distance where
 instance UnitNameAlt u => UnitNameAlt (Distance u) where
   unitNameAlt = unitNameAlt @u -- Type Application
 
-instance UnitNameAlt u => Show (Distance u)
- where
+instance UnitNameAlt u => Show (Distance u) where
   show (Distance t) = show t ++ "°" ++ unitNameAlt @u
 
 unitAlt ::
@@ -113,7 +101,6 @@ unitAlt ::
   => Distance u
   -> String
 unitAlt _ = unitNameAlt @u
-
 
 -- split line
 addMilesToKilometers ::
@@ -152,14 +139,11 @@ tenKilometers = Distance 10 :: Distance Kilometer
 
 a = addDistance tenMiles tenMiles
 
-
 -- => Distance {unDistance = 20.0} :: Distance Mile
 b = addDistance tenKilometers tenKilometers
 
-
 -- => Distance {unDistance = 20.0} :: Distance Kilometer
 c = addDistance tenMiles tenKilometers
-
 
 -- => Distance {unDistance = 26.0934} :: Distance Kilometer
 d = addDistance tenKilometers tenMiles
