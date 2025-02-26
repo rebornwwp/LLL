5090762bf4d35d78d9d1c406d027d54e774e3a4e
MC_pengzi <rebornwwp@gmail.com>
2025-02-26 15:34:58 +0800

    fix: add

--- zig/exercises/exercises/025_errors5.zig
+++ zig/exercises/exercises/025_errors5.zig
@@ -26,7 +26,7 @@ fn addFive(n: u32) MyNumberError!u32 {
     // This function needs to return any error which might come back from detect().
     // Please use a "try" statement rather than a "catch".
     //
-    const x = detect(n);
+    const x = try detect(n);
 
     return x + 5;
 }
--- zig/exercises/exercises/059_integers.zig
+++ zig/exercises/exercises/059_integers.zig
@@ -20,9 +20,9 @@ const print = @import("std").debug.print;
 
 pub fn main() void {
     const zig = [_]u8{
-        0o131, // octal
-        0b1101000, // binary
-        0x66, // hex
+        'Z', // octal
+        'i', // binary
+        'g', // hex
     };
 
     print("{s} is cool.\n", .{zig});
