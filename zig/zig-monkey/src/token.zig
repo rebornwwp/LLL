0d27d2f475e96d507ed5b5beda271e6e29c0461e
mc_pengzi <rebornwwp@gmail.com>
2025-10-03 22:43:46 +0800

    fix: add

---
+++ zig/zig-monkey/src/token.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+const TokenType = enum([]u8) {
+    ILLEGAL = "ILLEGAL",
+    EOF = "EOF",
+
+    // Identifiers + literals
+    IDENT = "IDENT", // add, foobar, x, y, ...
+    INT = "INT",
+
+    // Operators
+    ASSIGN = "=",
+    PLUS = "+",
+    MINUS = "-",
+    BANG = "!",
+    ASTERISK = "*",
+    SLASH = "/",
+
+    LT = "<",
+    GT = ">",
+    EQ = "==",
+    NOTEQ = "!=",
+
+    // Delimiters
+    COMMA = ",",
+    SEMICOLON = ";",
+    LPAREN = "(",
+    RPAREN = ")",
+    LBRACE = "{",
+    RBRACE = "}",
+
+    // Keywords
+    // 1343456
+    FUNCTION = "FUNCTION",
+    LET = "LET",
+    TRUE = "TRUE",
+    FALSE = "FALSE",
+    IF = "IF",
+    ELSE = "ELSE",
+    RETURN = "RETURN",
+
+    // data type
+    STRING = "STRING",
+    LBRACKET = "[",
+    RBRACKET = "]",
+    COLON = ":",
+};
+
+const Token = struct {
+    tokenTp: TokenType,
+    literal: []const u8,
+};
