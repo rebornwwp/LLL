b3a045b6570bd3009fa36780b5f8a930763d238e
mc_pengzi <rebornwwp@gmail.com>
2025-10-16 01:02:24 +0800

    fix: add

--- zig/zig-monkey/src/lexer.zig
+++ zig/zig-monkey/src/lexer.zig
@@ -9,10 +9,48 @@ position: usize,
 readPosition: usize,
 ch: u8,
 
+fn isLetter(c: u8) bool {
+    return ('a' <= c and c <= 'z') || ('A' <= c and c <= 'Z') || c == '_';
+}
+
+fn isDigit(c: u8) bool {
+    return '0' <= c and c <= '9';
+}
+
 pub fn init(input: []const u8) Lexer {
     return Lexer{ .input = input };
 }
 
 pub fn nextToken(l: *Lexer) token.Token {
     _ = l;
+}
+
+pub fn readChar(l: *Lexer) void {
+    l.ch = if (l.readPosition >= l.input.len)
+        0
+    else
+        l.input[l.readPosition];
+    l.position = l.readPosition;
+    l.readPosition += 1;
+}
+
+pub fn peakChar(l: *Lexer) void {
+    _ = l;
+}
+
+pub fn readIdentifier(l: *Lexer) token.Token {
+    _ = l;
+}
+
+pub fn readNumber(l: *Lexer) token.Token {
+    _ = l;
+}
+pub fn readString(l: *Lexer) token.Token {
+    _ = l;
+}
+
+pub fn skipWhiteSpace(l: *Lexer) void {
+    while (l.ch == ' ' or l.ch == '\n' or l.ch == '\t' or l.ch == '\r') {
+        l.readChar();
+    }
 }
