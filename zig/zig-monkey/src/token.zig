15b9607d9352e9753c6f2ba589848417b916602e
u <rebornwwp@gmail.com>
2025-10-17 18:26:53 +0800

    add

--- zig/zig-monkey/src/token.zig
+++ zig/zig-monkey/src/token.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 const TokenType = enum([]u8) {
     ILLEGAL = "ILLEGAL",
@@ -57,3 +58,35 @@ pub fn newToken(tk: TokenType, l: []const u8) Token {
         .literal = l,
     };
 }
+
+const KeyWords = struct {
+    allocator: Allocator,
+    kws: std.AutoHashMap([]const u8, TokenType),
+
+    const Self = @This();
+    pub fn init(allocator: Allocator) *Self {
+        var self = try allocator.create(Self);
+        self.allocator = allocator;
+        self.kws = std.AutoArrayHashMap([]const u8, TokenType).init(allocator);
+        self.kws.put("let", TokenType.LET);
+        self.kws.put("fn", TokenType.FUNCTION);
+        self.kws.put("true", TokenType.TRUE);
+        self.kws.put("false", TokenType.FALSE);
+        self.kws.put("if", TokenType.IF);
+        self.kws.put("else", TokenType.ELSE);
+        self.kws.put("return", TokenType.RETURN);
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.kws.deinit();
+        self.allocator.destroy(self);
+    }
+
+    pub fn LookupKey(self: *Self, ident: []const u8) TokenType {
+        if (self.kws.get(ident)) |tkTp| {
+            return tkTp;
+        }
+        return TokenType.IDENT;
+    }
+};
